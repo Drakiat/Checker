1c0503c5582243f91e32eba6ad5de770ca2e883f
Drakiat <felipetapia@hotmail.fr>
2024-03-07 12:36:44 -0600

    modified:   scanner.py

--- scanner.py
+++ scanner.py
@@ -6,7 +6,7 @@ def scan_ips(ip_addresses, ports):
     # Convert list of IP addresses to a comma-separated string
     ip_addresses_str = " ".join(ip_addresses)
     ports_str = ','.join([str(i) for i in ports])
-    res = nm.scan(ip_addresses_str, arguments='-p {0} --min-rate 4500 --max-rtt-timeout 1500ms --open'.format(ports_str),timeout=60)
+    res = nm.scan(ip_addresses_str, arguments='-Pn -p {0} --min-rate 4500 --max-rtt-timeout 1500ms --open'.format(ports_str),timeout=60)
     for ip in res['scan']:
         open_ports[ip] = []
         for port in res['scan'][ip]['tcp']:
